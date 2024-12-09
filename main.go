e5e70cf07b93dc2a8a209f66042489095948a881
Patrick <queuedelivery@gmail.com>
2024-12-09 16:20:40 -0500

    Bug fixes with dependency injection

--- main.go
+++ main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"data-ingestor/config"
 	"data-ingestor/dbtransposer"
 	"data-ingestor/fileloader"
 	"data-ingestor/mapreduce"
+	"database/sql"
 	"flag"
+	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib" // PostgreSQL driver
+	"go.uber.org/zap"
 	"log"
 )
 
+type App struct {
+	Config *config.Config
+	Logger    *zap.Logger
+	DB *sql.DB
+}
+
+// NewApp initializes the App with dependencies
+func NewApp() (*App, error) {
+	cfg := config.GetConfig()
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, fmt.Errorf("fatal error initializing logger: %w", err)
+	}
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", cfg.DB.DBUser, cfg.DB.DBPassword, cfg.DB.DBHostname, cfg.DB.DBPort, cfg.DB.DBName)
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("fatal error connecting to database: %w", err)
+	}
+
+	// Test the connection
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping after connecting to database: %w", err)
+	}
+
+	db.SetMaxOpenConns(cfg.Runtime.WorkerCount)
+	return &App{Config: cfg, Logger: logger, DB: db}, nil
+}
 
 func main() {
 	app, err := NewApp()
@@ -53,3 +86,7 @@ func main() {
 
 }
 
+func (app *App) Close() {
+	app.Logger.Sync()
+	app.DB.Close()
+}
